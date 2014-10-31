8a7a081ab75725648708f9f5beb0c060c3e77026
Nikita <xnimorz@gmail.com>
2014-11-01 01:49:06 +0300

    [UIPopup] new component

--- UIPopup/example.js
+++ UIPopup/example.js
@@ -2,4 +2,9 @@ $(function() {
     new UIPopup('.show-popup', {
         contentEl: '.js-popup'
     });
+
+    new UIPopup('.js-popup-open-fade', {
+        contentEl: '.js-popup-fade',
+        isFadeScreen: true
+    });
 });
