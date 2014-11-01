823195d192c2056b87072b96151929372356df5b
Nikita <xnimorz@gmail.com>
2014-11-01 16:28:19 +0300

    [UITooltip] new component

--- UITooltip/UITooltip.js
+++ UITooltip/UITooltip.js
@@ -1,20 +1,25 @@
-$(function(){
-    window.UITooltip = UITooltip;
-
-    var templates = {
-        tooltip: '<div class="UITooltip"></div>'
+(function($) {
+    var template = {
+        tooltip: '<div class="tooltip"></div>'
     };
 
+    /**
+     * UITooltip принимает следующие параметры
+     * {String} content - контент тултипа
+     * {Number} timeout - задержка до появления тултипа
+     */
     var options = {
         content: "",
-        timeout: 0
+        timeout: 100
     };
+
+    var TOOLTIP_TOP_OFFSET = 14;
 
     function UITooltip(element, settings) {
         this.$element = $(element);
         this.options = $.extend({}, options, settings);
 
-        this.$tooltip = $(templates.tooltip);
+        this.$tooltip = $(template.tooltip);
         this.$element.parent().append(this.$tooltip);
         this.$tooltip.html(this.options.content).hide();
         this.handle = 0;
@@ -25,28 +30,11 @@ $(function(){
                 setTimeout(function(){
                     this.$tooltip.css({
                         left: function() {
-                            var limit = $(window).width();
-                            if (this.$tooltip.width() >= limit) {
-                                this.$tooltip.css({width: this.$element.width()});
-                            }
-
-                            if (this.$element.width() >= this.$tooltip.width()) {
-                                return this.$element.position().left + (this.$element.width() - this.$tooltip.width()) / 2;
-                            }
-                            else {
-                                var left = this.$element.offset().left;
-                                var offset = (this.$tooltip.width() - this.$element.width()) / 2;
-                                if (offset < left && left - offset + this.$tooltip.width() < limit) {
-                                    return left - offset + (this.$tooltip.width() / 2);
-                                }
-                                else {
-                                    console.log('Unsupported');
-                                }
-                            }
+                            var elementMiddle = this.$element.outerWidth() / 2;
+                            return elementMiddle - this.$tooltip.outerWidth() / 2;
                         }.bind(this),
-                        top: this.$element.position().top + this.$element.height() + 14
+                        top: this.$element.position().top + this.$element.height() + TOOLTIP_TOP_OFFSET
                     });
-                    console.log(this.$tooltip.position());
                 }.bind(this), 0);
              }.bind(this), this.options.timeout);
         }.bind(this));
@@ -57,19 +45,23 @@ $(function(){
         }.bind(this));
     }
 
-    $('[data-tooltip]').each(function() {
-        new UITooltip(this, {content: $(this).attr('data-tooltip')});
+    (function(UITooltip) {
+        if (typeof define === 'function' && define.amd) {
+            //AMD. Анонимный модуль
+            define(UITooltip);
+        } else if (typeof exports === 'object') {
+            //CommonJS
+            module.exports = UITooltip;
+        } else {
+            //Глобальный scope
+            window.UITooltip = UITooltip;
+        }
+    })(UITooltip);
+
+    $(function() {
+        $('[data-tooltip]').each(function() {
+            new UITooltip(this, {content: $(this).attr('data-tooltip')});
+        });
     });
 
-//    var $tooltip = $('[data-tooltip]');
-//    $tooltip.hover(function(){
-//        var $tooltip = $(this).append(templates.tooltip).children('.UITooltip');
-//        $tooltip.html($(this).attr('data-tooltip')).css({
-//            left: (Math.abs(($(this).width() - $tooltip.width())) / 2) + 'px',
-//            top: '14px'
-//        });
-//    }, function(){
-//        $('.UITooltip', this).remove();
-//    });
-
-});
+})(jQuery);
---
+++ UITooltip/example.js
@@ -0,0 +1,3 @@
+$(function() {
+    new UITooltip('.js-tooltip', {content: 'js tooltip content', timeout: 100});
+});
