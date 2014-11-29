c3d5b2f5fa3915a511b201e1e603fd9ea16ff073
Nikita <xnimorz@gmail.com>
2014-11-29 11:42:17 +0300

    [require] add require packages

---
+++ require.config.js
@@ -0,0 +1,39 @@
+require.config({
+    packages: [
+        {
+            name: 'UIComponents/UIDropdown',
+            location: 'UIDropdown/UIDropdown',
+            main: 'Dropdown'
+        },
+        {
+            name: 'UIComponents/UICarriage',
+            location: 'UICarriage/UICarriage',
+            main: 'Carriage'
+        },
+        {
+            name: 'UIComponents/UIPopup',
+            location: 'UIPopup/UIPopup',
+            main: 'Popup'
+        },
+        {
+            name: 'UIComponents/UICalendar',
+            location: 'UICalendar/UICalendar',
+            main: 'Calendar'
+        },
+        {
+            name: 'UIComponents/UIFormStorage',
+            location: 'UIFormStorage/UIFormStorage',
+            main: 'FormStorage'
+        },
+        {
+            name: 'UIComponents/UIHint',
+            location: 'UIHint/UIHint',
+            main: 'Hint'
+        },
+        {
+            name: 'UIComponents/UIPageScrolling',
+            location: 'UIPageScrolling/UIPageScrolling',
+            main: 'PageScrolling'
+        }
+    ]
+});
